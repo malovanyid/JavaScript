b7d7c1f2267356f558a5fb26f207123c8cf49f19
malovanyid <malovanyid@gmail.com>
2024-08-06 18:10:13 +0300

    init commit

--- HomeWork_01/HW_01.js
+++ HomeWork_01/HW_01.js
@@ -1,34 +1,34 @@
 // - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
 // Вивести кожну змінну за допомогою: console.log
 let text1 = 'hello';
-console.log(text1)
+console.log(text1);
 let text2 = 'owu';
-console.log(text2)
+console.log(text2);
 let text3 = 'com';
-console.log(text3)
+console.log(text3);
 let text4 = 'ua';
-console.log(text4)
+console.log(text4);
 
 let numb1 = 1;
-console.log(numb1)
+console.log(numb1);
 let numb2 = 10;
-console.log(numb2)
+console.log(numb2);
 let numb3 = -999;
-console.log(numb3)
+console.log(numb3);
 let numb4 = 123;
-console.log(numb4)
+console.log(numb4);
 let numb5 = 3.14;
-console.log(numb5)
+console.log(numb5);
 let numb6 = 2.7;
-console.log(numb6)
+console.log(numb6);
 let numb7 = 16;
-console.log(numb7)
+console.log(numb7);
 
 let bool1 = true;
-console.log(bool1)
+console.log(bool1);
 
 let bool2 = false;
-console.log(bool2)
+console.log(bool2);
 
 // - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
 
