5c0ab48ddba46c0ef2c9cbddf7389c45483c49c8
malovanyid <malovanyid@gmail.com>
2024-09-03 08:12:16 +0300

    added a control

--- HomeWork_07/HW_07.js
+++ HomeWork_07/HW_07.js
@@ -1,6 +1,22 @@
 // #XjJuucOMR0
 // - Створити функцію конструктор для об'єктів User з полями id, name, surname , email, phone
 // створити пустий масив, наповнити його 10 об'єктами new User(....)
+function User (name, age, status) {
+    this.name = name;
+    this.age = age;
+    this.status = status;
+}
+let user1 = new User ('Denis', 38, true);
+let user2 = new User ('Andrey', 42, true);
+let user3 = new User ('Yura', 40, true);
+let user4 = new User ('Nik', 33, true);
+let user5 = new User ('Anna', 33, true);
+let user6 = new User ('Alex', 36, true);
+let user7 = new User ('Anton', 36, false);
+let user8 = new User ('Nastya', 38, true);
+let user9 = new User ('Bob', 24, false);
+let user10 = new User ('Cocos', 2, false);
+console.log([user1, user2, user3, user4, user5, user6, user7, user8, user9, user10]);
 //
 // #2ikXsE2WiKZ
 // - Взяти масив з  User[] з попереднього завдання, та відфільтрувати , залишивши тільки об'єкти з парними id (filter)
